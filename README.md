fa4bdf70e0118ca0bb6568455d6553769790f0f9
Dennis Iversen <deiv@aarhus.dk>
2023-02-21 10:44:01 +0100

    fix delay. Clean up

--- README.md
+++ README.md
@@ -4,18 +4,27 @@ Install:
 
     npm install -g simple-file-watch
 
-Simple nodejs cli command that will watch for changes rescursively in a directory,
-and execute a command (optional) on changes. 
+Simple nodejs cli command that will watch for changes in a directory or directories, recursively or not,
+and execute a command on changes. The delay set by `--delay` will prevent any
+execution of the command until the delay has passed. This is useful if the command
+being executed alters another file in the directory being watched. Otherwise there will be
+an infinite loop of file changes and executions.
 
-Usage example: 
+Usage example:
 
-    simple-file-watch --extension='js,php' --path='.' --path='test' --command='ls -l' --silence
+    simple-file-watch --delay=2000 --extension='js' --path='.' --path='test' --command='ls -l' --recursive --silence
+
+Flags:
+
+    --delay=2000 // delay in milliseconds before executing command on file change
+    --extension='js' // only execute command on files with this extension. Default is all files
+    --path='.' // path to watch for changes. Multiple paths are allowed. Default is current directory
+    --command='ls -l' // command to execute on file change  
+    --recursive // watch subdirectories
+    --silence // no output to stdout
 
 If you run the command without any params, it will watch any file changes in 
-current directory, and output some info to stdout. If you specify `command`
-then the the given command will be executed on file changes. If you specify `extension`
-then the `command` will only be executed on changes to files with the given
-extension.
+current directory, and output the file changed to stdout. 
 
 Note: There is max on number of files you can watch. See: 
 
--- cli.js
+++ cli.js
@@ -5,8 +5,6 @@ opts.boolean = ['help', 'silence', 'h'];
 opts.string = ['path', 'extension', 'command'];
 
 var m = require('minimist-mini')(opts);
-
-var fs = require('fs');
 var path = require('path');
 var fileExtension = require('file-extension');
 var exec = require('child_process').exec;
@@ -23,14 +21,26 @@ if (!path) {
 }
 
 var extension = m.get('extension');
-if (extension){
+if (extension) {
     extension = extension.split(",");
 } else {
     extension = '*';
 }
 
+var delay = m.get('delay');
+if (!delay) {
+    delay = 200;
+}
+
+var recursive = m.get('recursive');
+if (recursive) {
+    recursive = true;
+} else {
+    recursive = false;
+}
+
 // Log messages
-function consoleLog (txt) {
+function consoleLog(txt) {
     if (m.get('silence')) {
         return;
     }
@@ -38,38 +48,49 @@ function consoleLog (txt) {
 }
 
 function execute(cmd) {
+
     exec(cmd, function (error, stdout, stderr) {
         if (error) {
             // Always log error
             console.log(error);
         }
-        
+
         if (stderr) {
             // Always log error
             console.log(stderr);
         }
-        
+
         consoleLog(stdout);
     });
 }
 
 var command = m.get('command');
-watch(path, function (filename) {
+var lastExecution = new Date().getTime();
 
-    if (filename) {
-        var ext = fileExtension(filename);
-        if (extension && (extension.indexOf(ext) !== -1)) {
-            consoleLog("File changed: " + filename);
-            if (command){ 
-                consoleLog("Executing: " + command);
-                execute(command);
-            }
-        } else {
-            consoleLog("File changed: " + filename);
-            if (command && (extension === '*')){ 
-                consoleLog("Executing: " + command);
-                execute(command);
-            }
-        }        
+watch(path, { delay: delay, recursive: recursive, persistent: true }, async function (evt, filename) {
+
+    // Check if last execution was less than 'delay' ago + some offset (10% of delay)
+    // This is to avoid multiple executions when multiple files are changed at the same time
+    var now = new Date().getTime();
+    if (now - lastExecution < delay + parseInt(delay / 10)) {
+        return;
     }
+
+    var ext = fileExtension(filename);
+    if (extension && (extension.indexOf(ext) !== -1)) {
+        consoleLog("File changed: " + filename);
+        if (command) {
+            consoleLog("Executing: " + command);
+            execute(command);
+        }
+    } else {
+        consoleLog("File changed: " + filename);
+        if (command && (extension === '*')) {
+            consoleLog("Executing: " + command);
+            execute(command);
+        }
+    }
+
+    lastExecution = new Date().getTime();
+
 });
